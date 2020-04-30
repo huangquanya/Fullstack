dc121206a6511fd2b895fdde5445ceb3e90c1d00
huangquanya <1036871949@qq.com>
2020-04-30 21:54:18 +0800

    react初第一次学习

---
+++ hallow react/README.md
@@ -0,0 +1,9 @@
+- react JSX语法， 将网页 模块化分离开发，
+    函数 返回 html片段 Component
+    网页由conponent组成  拼积木一样简单
+    jquery dom 更友好一些 react 新的创新
+    1. es6 import export
+    2. 写一个函数， 函数的返回值是JSX js里写html
+
+- 用react 做项目 七天 UI 不再是DOM 而是组件
+- 新的UI 
---
+++ hallow react/src/CommentList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function CommentList(){
+    return(
+        <ul>
+            <li>
+                用户名：1221
+            </li>
+            <li>
+                用户名：1221
+            </li>
+            <li>
+                用户名：1221
+            </li>
+            <li>
+                用户名：1221
+            </li>
+        </ul>
+    )
+}
+
+export default CommentList;
