507b017f91413d8e53d1b2b8990b01ba2198fcbb
huangquanya <1036871949@qq.com>
2020-07-04 15:37:22 +0800

    react-redux 学习 无状态组件与stateToProps和dispatchToProps

--- react/redux-demo02/README.md
+++ react/redux-demo02/README.md
@@ -1,1 +1,3 @@
 jspang react-redux 教程
+
+connect的作用是把UI组件（无状态组件）和业务逻辑代码的分开，然后通过connect再链接到一起，让代码更加清晰和易于维护。这也是React-Redux最大的有点。
--- react/redux-demo02/src/TodoList.js
+++ react/redux-demo02/src/TodoList.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
-import store from './store'
+
 import {connect} from 'react-redux'
 
-class TodoList extends Component {
-    constructor(props){
-        super(props)
-        this.state = store.getState()
-    }
-    render() { 
-        return (
+const TodoList = (props)=>{
+    let { inputValue, inputChange, clickButton, list } = props;
+    return (
+        <div>
             <div>
-                <div>
-                    <input value={this.props.inputValue} onChange={this.props.inputChange} />
-                    <button onClick={this.props.clickButton}>提交</button>
-                </div>
-                <ul>
-                    {
-                        this.props.list.map((item,index)=>{
-                        return (<li key={index}>{item}</li>)
-                        })
-                    }
-                </ul>
+                <input value={inputValue} onChange={inputChange} />
+                <button onClick={clickButton}>提交</button>
             </div>
-            );
-    }
+            <ul>
+                {
+                    list.map((item,index)=>{
+                    return (<li key={index}>{item}</li>)
+                    })
+                }
+            </ul>
+        </div>
+        );
 }
+//     render() { 
+//         return (
+//             <div>
+//                 <div>
+//                     <input value={inputValue} onChange={inputChange} />
+//                     <button onClick={clickButton}>提交</button>
+//                 </div>
+//                 <ul>
+//                     {
+//                         list.map((item,index)=>{
+//                         return (<li key={index}>{item}</li>)
+//                         })
+//                     }
+//                 </ul>
+//             </div>
+//             );
+//     }
+// }
 const stateToProps = (state)=>{
     return {
         inputValue : state.inputValue,
