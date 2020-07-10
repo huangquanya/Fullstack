7573b630ad0e041e2356cbc4255eb0d9e287eab7
huangquanya <1036871949@qq.com>
2020-07-10 16:09:23 +0800

    leetcode每日刷题

--- LEETCODE/136/136.js
+++ LEETCODE/136/136.js
@@ -1,20 +1,26 @@
+// 解法思路1 不合空间复杂度要求
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
+ * @param {number[]} nums
+ * @return {number}
  */
+var singleNumber = function(nums) {
+    let res = []
+    nums.map(i=>{
+        if(!(res.indexOf(i)+1)){
+            res.push(i)
+        }else{
+            res.splice(res.indexOf(i),1)
+        }
+    })
 
+    return res[0]
+};
+
+// 解法2
 /**
- * @param {ListNode} head
- * @return {boolean}
+ * @param {number[]} nums
+ * @return {number}
  */
-var hasCycle = function(head) {
-    while(head){
-        if(head.flag)return true;
-        head.flag = 1;
-        head = head.next;
-    }
-    return false;
+var singleNumber = function(nums) {
+    return nums.reduce((a,b)=>a^b)
 };
