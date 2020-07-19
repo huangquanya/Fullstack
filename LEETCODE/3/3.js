f5ecf6dfba60273c8de2913cc661462ab7ec36e6
huangquanya <1036871949@qq.com>
2020-07-19 10:00:21 +0800

    每日力扣 一道

---
+++ LEETCODE/3/3.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let arr = [];
+    let max = 0;
+    for(let i = 0;i<s.length;i++){
+        if(arr.indexOf(s[i])!==-1){
+            arr.splice(0,arr.indexOf(s[i])+1)
+        }
+        arr.push(s[i])
+        max = Math.max(max,arr.length)
+    }
+    return max
+};
